2c52192f71d94b774ccf3bae988ba8413a51130c
Adam Moore <adam@trainerroad.com>
2025-03-07 14:33:38 -0800

    Update layout.js

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,13 +2,13 @@ import PropTypes from "prop-types";
 import React from "react";
 import Header from "../header/header";
 import Social from "../social";
-import styles from "./layout.scss";
+import "./layout.scss";
 
 const Layout = ({ children }) => {
     return (
         <>
             <Header />
-            <div className={styles["outer-padding"]}>
+            <div className="outer-padding">
                 <main>{children}</main>
             </div>
             <footer>
